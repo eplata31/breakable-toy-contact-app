274d867bd718fa4f7b54f7eae75f345fdb3cc7b8
eplata31 <eplata31@outlook.com>
2019-03-23 14:17:22 -0700

    Add handling errors

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,35 +1,55 @@
 const Contact = require('../models/contact')
 
-async function getAllContacts(ctx) {
-    const contacts = await Contact.find({})
-    ctx.body = contacts
+async function getAllContacts(ctx){
+    try {
+        const contacts = await Contact.find({})
+        ctx.body = contacts
+    } catch (error) {
+        console.log('Error getting all contacts: ', error)
+    }
 }
 
-async function getContact(ctx) {
-    const name = ctx.params.contactId
-    var filter = new RegExp(name, "i")
-    const contact = await Contact.find({ $or: [{ 'name': filter }, { 'lastname': filter }] }, 'name lastname')
-    ctx.body = contact
+async function getContact(ctx){
+    try {
+        const name = ctx.params.contactId
+        var filter = new RegExp(name, "i")
+        const contact = await  Contact.find({ $or:[{'name':filter}, {'lastname':filter} ]}, 'name lastname')
+        ctx.body = contact
+    } catch (error) {
+        console.log('Error finding contact: ', error)
+    }
 }
 
-async function createContact(ctx) {
-    const newContact = ctx.request.body
-    let contact = new Contact(newContact)
-    const savedContact = await contact.save()
-    console.log('Contact saved!')
-    ctx.body = savedContact
+async function createContact(ctx){
+    try {
+        const newContact = ctx.request.body
+        let contact = new Contact(newContact)
+        const savedContact = await contact.save()
+        console.log('Contact saved!')
+        ctx.body = savedContact
+    } catch (error) {
+        console.log('Error saving contact: ', error)
+    }
 }
 
-async function updateContact(ctx) {
-    const id = ctx.params.contactId
-    const updateContact = await Contact.findByIdAndUpdate(id, ctx.request.body)
-    ctx.body = updateContact
+async function updateContact(ctx){
+    try {
+        const id = ctx.params.contactId
+        const updateContact = await Contact.findByIdAndUpdate(id, ctx.request.body)
+        ctx.body = updateContact
+    } catch (error) {
+        console.log('Error updating contact: ', error)
+    }
 }
 
-async function deleteContact(ctx) {
-    const id = ctx.params.contactId
-    const deleteContact = await Contact.findByIdAndRemove(id)
-    ctx.body = deleteContact
+async function deleteContact(ctx){
+    try {
+        const id = ctx.params.contactId
+        const deleteContact = await Contact.findByIdAndRemove(id)
+        ctx.body = deleteContact
+    } catch (error) {
+        console.log('Error deleting contact: ', error)
+    }
 }
 
 module.exports = {
